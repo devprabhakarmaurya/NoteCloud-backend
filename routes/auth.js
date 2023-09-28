f80d5a146429c7c700e1d246e66a020b11fcb19a
Prabhakar <techprabhakarmaurya@gmail.com>
2023-09-28 21:15:01 +0530

    Added Route for login

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-//Create a user with post that doesn't require auth api = "/api/auth/createuser"
+//ROUTE 1: Create a user with post that doesn't require auth POST- "/api/auth/createuser" No login required
 router.post('/createuser', [
     body("name").isString().isLength({ min: 3 }).withMessage("Name should be atleast 3 Characters"),
     body("email").notEmpty().isEmail().withMessage("Please enter valid email"),
@@ -28,7 +29,7 @@ router.post('/createuser', [
 
             // Generate a JWT token
             const token = jwt.sign({user: user._id}, process.env.JWT_SIGN)
-            res.json({ message: 'User created successfully', user, token });
+            res.json({ message: 'User created successfully', token });
         }
         else {
             return res.status(400).json({ error: "Sorry user exists with this email" });
@@ -39,5 +40,43 @@ router.post('/createuser', [
     }
 
 
+})
+//ROUTE 2:  Authenticate the user with POST /api/auth/login No login required
+router.post('/login', [
+    body("email").notEmpty().isEmail().withMessage("Please enter valid email"),
+    body("password").notEmpty().withMessage("Password should not be blank"),
+
+], async (req, res) => {
+    // errs in validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    //destructuring req.body 
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({email})
+        if(user){
+            //comparing given password with user's password
+            const passwordCompare = await bcrypt.compare(password , user.password);
+            if(passwordCompare){
+                //Generating auth token 
+                const token = jwt.sign({user: user._id}, process.env.JWT_SIGN)
+                //sending token as response
+                res.json({ message: 'User Login Successfuly', token });
+            }
+            else{
+                // Bad request with error when password is wrong
+                return res.status(400).json({ error: "Invalid Credential" });
+            }
+        }
+        else{
+             // Bad request with error when email is wrong
+            return res.status(400).json({ error: "Invalid Credential" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
 })
 module.exports = router;
