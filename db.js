9210d12ce5cce0232fbd544553cdcae3c2b4ada9
Prabhakar <techprabhakarmaurya@gmail.com>
2023-10-07 13:19:53 +0530

    changes for deployement

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.Mongo_URI;
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () =>{
 mongoose.connect(mongoURI, { 
         useNewUrlParser: true,
         useUnifiedTopology: true 
     });
+    console.log(mongoURI);
 console.log("Connected to Mongo Succesfully")
 }
 
